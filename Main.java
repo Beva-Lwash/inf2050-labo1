08a37c05a97c9aa74f0438deb368631087334ce4
Beva-Lwash <seye.ahmadou@courrier.uqam.ca>
2019-01-18 15:22:58 -0500

    This commit will mentionned that the files have been modified

--- Main.java
+++ Main.java
@@ -6,7 +6,7 @@ public static void main(String args[]){
 
 		if (i%3==0){	
 		System.out.println(" Fizz ");
-		}else if(i%5==0) {            
+		}elseert if(i%5==0) {            
 		System.out.println(" Buzz ");
 		}else if(i%15 == 0 ){			
 		System.out.println(" FizzBuzz");
