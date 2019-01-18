b8565b70f4600f87375a53688d02e278ed01b0f6
Beva-Lwash <seye.ahmadou@courrier.uqam.ca>
2019-01-18 15:19:49 -0500

    This commit will mentionned that the files have been modified

--- Main.java
+++ Main.java
@@ -1,14 +1,14 @@
 public class Main{
 public static void main(String args[]){
 
-		int nombre =100;
+		int nombre =200;
 		for(int i =1; i<=nombre; i++){
 
 		if (i%3==0){	
 		System.out.println(" Fizz ");
-		}if(i%5==0) {            
+		}else if(i%5==0) {            
 		System.out.println(" Buzz ");
-		}if(i%15 == 0 ){			
+		}else if(i%15 == 0 ){			
 		System.out.println(" FizzBuzz");
 		}			
 
